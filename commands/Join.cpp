e3780e0dc31d140fd203b0cd71d1223af5b7294d
Oleg Goman <ogoman@c1r6p2.hive.fi>
2025-02-04 07:49:13 +0200

    JOIN

--- commands/Join.cpp
+++ commands/Join.cpp
@@ -1,10 +1,7 @@
 #include "Join.hpp"
-
 #include <sys/socket.h>
-
 #include <iostream>
 #include <string>
-
 #include "../include/Channel.hpp"
 #include "../include/Server.hpp"
 
@@ -57,7 +54,11 @@ void handleJoinCommand(Server* server, int fd,
 
     // Look for the channel in the server's channel map.
     auto it = server->_channels.find(channelName);
-    if (it == server->_channels.end())
+
+    // Check if this is the first user to join the channel
+    bool isFirstUser = (it == server->_channels.end());
+
+    if (isFirstUser)
     {
         // If the channel does not exist, create a new one.
         auto emplaceResult =
@@ -97,175 +98,43 @@ void handleJoinCommand(Server* server, int fd,
     // Add the client to the channel.
     it->second.addClient(fd);
 
-    // Compose a JOIN message to notify all other members of the channel.
-    std::string joinMsg =
-        ":" + server->_clients[fd]->nickname + " JOIN " + channelName + "\r\n";
-    for (int cli_fd : it->second.getClients())
-    {
-        // Optionally, do not send the JOIN message to the joining client.
-        if (cli_fd != fd) send(cli_fd, joinMsg.c_str(), joinMsg.size(), 0);
+    // If this is the first user, assign them as an operator.
+    if (isFirstUser) {
+        it->second.addOperator(fd);
+        std::string opMsg = ":" + server->_clients[fd]->nickname + " MODE " + channelName + " +o " + server->_clients[fd]->nickname + "\r\n";
+        send(fd, opMsg.c_str(), opMsg.size(), 0);
+    }
+
+    // If this is the first user, send a welcome message 
+    std::string welcomeMsg = "NOTICE " + channelName + " :Welcome to " + channelName;
+    if (isFirstUser) {
+        welcomeMsg += "! You are the first user and the operator.";
+    }
+    welcomeMsg += "\r\n";
+    send(fd, welcomeMsg.c_str(), welcomeMsg.size(), 0);
+    
+    // Send list of users to the new client
+    std::string userList = "353 " + server->_clients[fd]->nickname + " = " + channelName + " :";
+    for (int cli_fd : it->second.getClients()) {
+        userList += server->_clients[cli_fd]->nickname + " ";
+    }
+    userList += "\r\n";
+    send(fd, userList.c_str(), userList.size(), 0);
+
+    // Send JOIN confirmation to the new client
+    std::string joinMsg = ":" + server->_clients[fd]->nickname + " JOIN " + channelName + "\r\n";
+    send(fd, joinMsg.c_str(), joinMsg.size(), 0);
+
+    // Send channel topic if it is set
+    if (!it->second.getTopic().empty()) {
+        std::string topicMsg = "332 " + server->_clients[fd]->nickname + " " + channelName + " :" + it->second.getTopic() + "\r\n";
+        send(fd, topicMsg.c_str(), topicMsg.size(), 0);
+    }
+
+    // Notify other clients in the channel about the new user
+    for (int cli_fd : it->second.getClients()) {
+        if (cli_fd != fd) {
+            send(cli_fd, joinMsg.c_str(), joinMsg.size(), 0);
+        }
     }
 }
-
-// #include <stdexcept>
-// #include <iostream>
-// #include <vector>
-// #include <string>
-// #include <map>
-// #include "../include/Server.hpp"
-// #include "../include/Channel.hpp"
-
-// // Helper: sends an error to a client
-// void sendErrorMessage(int fd, const std::string& message) {
-//     send(fd, message.c_str(), message.size(), 0);
-// }
-
-// // Check if the client is fully registered
-// bool isClientRegistered(Server* server, int fd) {
-//     if (server->_clients[fd]->authState != AUTH_REGISTERED) {
-//         sendErrorMessage(fd, "451 :You have not registered\r\n");
-//         return false;
-//     }
-//     return true;
-// }
-
-// // Validate "JOIN <channel> [<key>]"
-// bool isJoinCommandValid(int fd, const std::vector<std::string>& tokens) {
-//     if (tokens.size() < 2) {
-//         sendErrorMessage(fd, "461 JOIN :Not enough parameters\r\n");
-//         return false;
-//     }
-//     return true;
-// }
-
-// // Create or find channel
-// Channel& getOrCreateChannel(Server* server, const std::string& channelName) {
-//     auto it = server->_channels.find(channelName);
-//     if (it == server->_channels.end()) {
-//         auto res = server->_channels.emplace(channelName,
-//         Channel(channelName)); if (!res.second) {
-//             std::cerr << "Failed to create channel: " << channelName <<
-//             std::endl; throw std::runtime_error("Channel creation failed");
-//         }
-//         it = res.first;
-//     }
-//     return it->second;
-// }
-
-// // Check invite-only, limit, etc. (but not the +k key yet)
-// bool basicChannelChecks(int fd, Channel& channel) {
-//     if (channel.isInviteOnly() && !channel.isOperator(fd)) {
-//         sendErrorMessage(fd, "473 " + channel.getName() + " :Cannot join
-//         channel (+i)\r\n"); return false;
-//     }
-//     if (channel.getUserLimit() > 0 && (int)channel.getClients().size() >=
-//     channel.getUserLimit()) {
-//         sendErrorMessage(fd, "471 " + channel.getName() + " :Channel is
-//         full\r\n"); return false;
-//     }
-//     return true;
-// }
-
-// /**
-//  * Notify all existing channel members that a new user has joined.
-//  * Usually: ":<nick> JOIN <channel>\r\n"
-//  */
-// void notifyChannelMembers(Server* server, int fd, Channel& channel) {
-//     std::string joinMsg = ":" + server->_clients[fd]->nickname +
-//                           " JOIN " + channel.getName() + "\r\n";
-
-//     for (int cli_fd : channel.getClients()) {
-//         // send to everyone (including the newcomer)
-//         send(cli_fd, joinMsg.c_str(), joinMsg.size(), 0);
-//     }
-// }
-
-// /**
-//  * Send 353/366 (NAME list, end of NAME list) to the newly joined user.
-//  */
-// void sendNameReply(int fd, Channel& channel, Server* server) {
-//     // Build user list
-//     std::string names;
-//     for (int memberFd : channel.getClients()) {
-//         names += server->_clients[memberFd]->nickname + " ";
-//     }
-//     // 353 <requestingNick> = #channel :names
-//     // example: "353 Alisa = #test :Bob Alisa"
-//     std::string rplNamReply = "353 " + server->_clients[fd]->nickname +
-//                               " = " + channel.getName() + " :" + names +
-//                               "\r\n";
-//     send(fd, rplNamReply.c_str(), rplNamReply.size(), 0);
-
-//     // 366 <requestingNick> #channel :End of /NAMES list
-//     std::string rplEndOfNames = "366 " + server->_clients[fd]->nickname +
-//                                 " " + channel.getName() +
-//                                 " :End of /NAMES list\r\n";
-//     send(fd, rplEndOfNames.c_str(), rplEndOfNames.size(), 0);
-// }
-
-// /**
-//  * Handle JOIN:
-//  *   1) Check registration
-//  *   2) Validate parameters
-//  *   3) get/create channel
-//  *   4) check +i, +l, etc.
-//  *   5) check +k if channel has a key
-//  *   6) add user
-//  *   7) notify members
-//  *   8) send name reply
-//  */
-// void handleJoinCommand(Server* server, int fd,
-//                        const std::vector<std::string>& tokens,
-//                        const std::string& rawCommand)
-// {
-//     (void)rawCommand; // not used, but might parse trailing for real IRC
-
-//     // 1) Check if registered
-//     if (!isClientRegistered(server, fd))
-//         return;
-
-//     // 2) "JOIN <channel> [<key>]"
-//     if (!isJoinCommandValid(fd, tokens))
-//         return;
-
-//     std::string channelName = tokens[1];
-
-//     // If user typed: JOIN #channel key
-//     // then tokens.size() could be >= 3, so tokens[2] is the key
-//     std::string userKey;
-//     if (tokens.size() >= 3) {
-//         userKey = tokens[2];
-//     }
-
-//     try {
-//         // 3) find or create
-//         Channel& channel = getOrCreateChannel(server, channelName);
-
-//         // 4) check +i, +l
-//         if (!basicChannelChecks(fd, channel)) {
-//             return;
-//         }
-
-//         // 5) check +k
-//         if (channel.hasMode('k')) {
-//             // compare userKey with channel.getChannelKey()
-//             if (userKey != channel.getChannelKey()) {
-//                 // wrong key or no key
-//                 std::string err = "475 " + channelName + " :Cannot join
-//                 channel (+k)\r\n"; sendErrorMessage(fd, err); return;
-//             }
-//         }
-
-//         // 6) add the user
-//         channel.addClient(fd);
-
-//         // 7) notify everyone
-//         notifyChannelMembers(server, fd, channel);
-
-//         // 8) send 353/366 to the new user
-//         sendNameReply(fd, channel, server);
-
-//     } catch (const std::exception& e) {
-//         std::cerr << "Error handling JOIN: " << e.what() << std::endl;
-//     }
-// }
