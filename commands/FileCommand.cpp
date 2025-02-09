414f1ed27eda84265e0a94a615a631611ecc305b
Alisa Arbenina <aarbenina@gmail.com>
2025-02-09 10:14:10 +0200

    [Alisa] FILE refactoring

--- commands/FileCommand.cpp
+++ commands/FileCommand.cpp
@@ -2,6 +2,7 @@
 #include "../include/Utils.hpp"
 #include "../include/Client.hpp"
 #include "../include/FileTransfer.hpp"
+
 #include <iostream>
 #include <cerrno>
 #include <unistd.h>
@@ -11,7 +12,7 @@
 
 /**
  * @brief Simple base64 decoder for demonstration. 
- *        No external library is used due to the project constraints.
+ *        No external library is used due to project constraints.
  */
 static std::vector<char> base64Decode(const std::string &in);
 
@@ -27,6 +28,203 @@ static std::string makeTransferKey(int senderFd,
     ss << senderFd << "_" << filename;
     return ss.str();
 }
+
+/* ------------------------------------------------------------------
+ * HELPER FUNCTIONS FOR EACH SUBCOMMAND
+ * ------------------------------------------------------------------ */
+
+/**
+ * @brief Handles "FILE SEND <nickname> <filename> <filesize>".
+ */
+static void handleFileSend(Server* server,
+                           int fd,
+                           const std::vector<std::string>& tokens)
+{
+    // We expect: tokens: [0]="FILE" [1]="SEND" [2]=nickname [3]=filename [4]=filesize
+    if (tokens.size() < 5) {
+        std::string err = "461 FILE SEND :Not enough parameters\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+
+    std::string targetNick  = tokens[2];
+    std::string filename    = tokens[3];
+    std::string filesizeStr = tokens[4];
+
+    size_t filesize = 0;
+    try {
+        filesize = static_cast<size_t>(std::stoul(filesizeStr));
+    } catch (...) {
+        std::string err = "461 FILE SEND :Invalid filesize\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+
+    // Look for receiver by nickname
+    int receiverFd = -1;
+    for (std::map<int, std::unique_ptr<Client> >::iterator it =
+             server->getClients().begin();
+         it != server->getClients().end(); ++it)
+    {
+        if (it->second->getNickname() == targetNick) {
+            receiverFd = it->first;
+            break;
+        }
+    }
+
+    if (receiverFd == -1) {
+        std::string err = "401 " + targetNick + " :No such nick\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+
+    // Build transfer key
+    std::string key = makeTransferKey(fd, filename);
+
+    // If a transfer with this key already exists, we can either overwrite or reject
+    if (server->getFileTransfers().count(key) != 0) {
+        server->getFileTransfers().erase(key);
+    }
+
+    // Create a new FileTransfer
+    FileTransfer ft(fd, receiverFd, filename, filesize);
+    server->getFileTransfers().insert(std::make_pair(key, ft));
+
+    // Notify the sender
+    {
+        std::string msg = "NOTICE " + targetNick +
+                          " :Ready to receive file '" + filename +
+                          "' (" + filesizeStr + " bytes)\r\n";
+        send(fd, msg.c_str(), msg.size(), 0);
+    }
+
+    // Notify the receiver
+    {
+        std::string msg = ":" + server->getClients()[fd]->getNickname() +
+                          " PRIVMSG " + targetNick +
+                          " :Incoming file: " + filename +
+                          " (" + filesizeStr +
+                          " bytes). Use FILE commands to handle.\r\n";
+        send(receiverFd, msg.c_str(), msg.size(), 0);
+    }
+}
+
+/**
+ * @brief Handles "FILE DATA <filename> <base64_chunk>".
+ */
+static void handleFileData(Server* server,
+                           int fd,
+                           const std::vector<std::string>& tokens)
+{
+    // We expect: tokens: [0]="FILE" [1]="DATA" [2]=filename [3]=base64_chunk
+    if (tokens.size() < 4) {
+        std::string err = "461 FILE DATA :Not enough parameters\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+
+    std::string filename    = tokens[2];
+    std::string base64chunk = tokens[3];
+
+    // Find the FileTransfer by key
+    std::string key = makeTransferKey(fd, filename);
+    if (server->getFileTransfers().count(key) == 0) {
+        std::string err = "400 :No such file transfer session\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+    
+    FileTransfer &ft = server->getFileTransfers()[key];
+
+    // Decode base64
+    std::vector<char> decodedData = base64Decode(base64chunk);
+
+    // Append to file buffer
+    ft.appendData(decodedData);
+
+    // Optionally notify about progress
+    {
+        std::ostringstream oss;
+        oss << "NOTICE "
+            << server->getClients()[fd]->getNickname()
+            << " :Uploaded " << ft.getReceivedBytes()
+            << "/" << ft.getFilesize() << " bytes of ["
+            << ft.getFilename() << "]\r\n";
+        std::string msg = oss.str();
+        send(fd, msg.c_str(), msg.size(), 0);
+    }
+}
+
+/**
+ * @brief Handles "FILE END <filename>".
+ */
+static void handleFileEnd(Server* server,
+                          int fd,
+                          const std::vector<std::string>& tokens)
+{
+    // We expect: tokens: [0]="FILE" [1]="END" [2]=filename
+    if (tokens.size() < 3) {
+        std::string err = "461 FILE END :Not enough parameters\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+    std::string filename = tokens[2];
+
+    std::string key = makeTransferKey(fd, filename);
+    if (server->getFileTransfers().count(key) == 0) {
+        std::string err = "400 :No such file transfer session\r\n";
+        send(fd, err.c_str(), err.size(), 0);
+        return;
+    }
+
+    FileTransfer &ft = server->getFileTransfers()[key];
+    bool complete = ft.isComplete();
+    int receiverFd = ft.getReceiverFd();
+
+    // Notify sender
+    if (!complete) {
+        std::string msgSender = "NOTICE " +
+            server->getClients()[fd]->getNickname() +
+            " :File transfer ended, but file is incomplete (" +
+            std::to_string(ft.getReceivedBytes()) + "/" +
+            std::to_string(ft.getFilesize()) + ")\r\n";
+        send(fd, msgSender.c_str(), msgSender.size(), 0);
+    } else {
+        std::string msgSender = "NOTICE " +
+            server->getClients()[fd]->getNickname() +
+            " :File transfer completed (" + ft.getFilename() + ")\r\n";
+        send(fd, msgSender.c_str(), msgSender.size(), 0);
+    }
+
+    // Send the file to the receiver (for demonstration).
+    {
+        const std::vector<char>& fileBuf = ft.getFileBuffer();
+
+        // A short info message to the receiver
+        std::ostringstream oss;
+        oss << "NOTICE "
+            << server->getClients()[receiverFd]->getNickname()
+            << " :You have received file ["
+            << ft.getFilename()
+            << "] with size " << fileBuf.size() << " bytes\r\n";
+        std::string infoMsg = oss.str();
+        send(receiverFd, infoMsg.c_str(), infoMsg.size(), 0);
+
+        // In a real scenario, we'd prefer DCC or other method for big files.
+        // Here, we just do a direct send of raw data (which may look like garbage
+        // in an IRC client). One could base64 it again, or do chunked sends.
+        if (!fileBuf.empty()) {
+            send(receiverFd, &fileBuf[0], fileBuf.size(), 0);
+        }
+    }
+
+    // Remove the transfer session
+    server->getFileTransfers().erase(key);
+}
+
+/* ------------------------------------------------------------------
+ * MAIN FILE COMMAND (DISPATCHER)
+ * ------------------------------------------------------------------ */
 
 /**
  * @brief Main entry point for handling FILE commands.
@@ -52,202 +250,25 @@ void handleFileCommand(Server* server,
     std::string subcmd = tokens[1];
     std::transform(subcmd.begin(), subcmd.end(), subcmd.begin(), ::toupper);
 
-    // ----------------------
-    // FILE SEND ...
-    // ----------------------
     if (subcmd == "SEND") {
-        // Format: FILE SEND <nickname> <filename> <filesize>
-        // tokens: [0]="FILE" [1]="SEND"
-        //         [2]=nickname [3]=filename [4]=filesize
-        if (tokens.size() < 5) {
-            std::string err = "461 FILE SEND :Not enough parameters\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-
-        std::string targetNick  = tokens[2];
-        std::string filename    = tokens[3];
-        std::string filesizeStr = tokens[4];
-
-        size_t filesize = 0;
-        try {
-            filesize = static_cast<size_t>(std::stoul(filesizeStr));
-        } catch (...) {
-            std::string err = "461 FILE SEND :Invalid filesize\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-
-        // Look for receiver by nickname
-        int receiverFd = -1;
-        for (std::map<int, std::unique_ptr<Client> >::iterator it =
-                 server->getClients().begin();
-             it != server->getClients().end(); ++it)
-        {
-            if (it->second->getNickname() == targetNick) {
-                receiverFd = it->first;
-                break;
-            }
-        }
-
-        if (receiverFd == -1) {
-            std::string err = "401 " + targetNick + " :No such nick\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-
-        // Build transfer key
-        std::string key = makeTransferKey(fd, filename);
-
-        // If a transfer with this key already exists, we can either overwrite or reject
-        if (server->getFileTransfers().count(key) != 0) {
-            server->getFileTransfers().erase(key);
-        }
-
-        // Create a new FileTransfer
-        FileTransfer ft(fd, receiverFd, filename, filesize);
-        server->getFileTransfers().insert(std::make_pair(key, ft));
-
-        // Notify the sender
-        {
-            std::string msg = "NOTICE " + targetNick +
-                              " :Ready to receive file '" + filename +
-                              "' (" + filesizeStr + " bytes)\r\n";
-            send(fd, msg.c_str(), msg.size(), 0);
-        }
-
-        // Notify the receiver
-        {
-            std::string msg = ":" + server->getClients()[fd]->getNickname() +
-                              " PRIVMSG " + targetNick +
-                              " :Incoming file: " + filename +
-                              " (" + filesizeStr +
-                              " bytes). Use FILE commands to handle.\r\n";
-            send(receiverFd, msg.c_str(), msg.size(), 0);
-        }
-    }
-
-    // ----------------------
-    // FILE DATA ...
-    // ----------------------
+        handleFileSend(server, fd, tokens);
+    } 
     else if (subcmd == "DATA") {
-        // Format: FILE DATA <filename> <base64_chunk>
-        // tokens: [0]="FILE" [1]="DATA"
-        //         [2]=filename [3]=base64_chunk
-        if (tokens.size() < 4) {
-            std::string err = "461 FILE DATA :Not enough parameters\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-
-        std::string filename    = tokens[2];
-        std::string base64chunk = tokens[3];
-        
-        std::string key = makeTransferKey(fd, filename);
-        if (server->getFileTransfers().count(key) == 0) {
-            std::string err = "400 :No such file transfer session\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-        
-        FileTransfer &ft = server->getFileTransfers()[key];
-
-        // Decode base64
-        std::vector<char> decodedData = base64Decode(base64chunk);
-
-        // Append to file buffer
-        ft.appendData(decodedData);
-
-        // Optionally notify about progress
-        {
-            std::ostringstream oss;
-            oss << "NOTICE "
-                << server->getClients()[fd]->getNickname()
-                << " :Uploaded " << ft.getReceivedBytes()
-                << "/" << ft.getFilesize() << " bytes of ["
-                << ft.getFilename() << "]\r\n";
-            std::string msg = oss.str();
-            send(fd, msg.c_str(), msg.size(), 0);
-        }
+        handleFileData(server, fd, tokens);
     }
-
-    // ----------------------
-    // FILE END ...
-    // ----------------------
     else if (subcmd == "END") {
-        // Format: FILE END <filename>
-        if (tokens.size() < 3) {
-            std::string err = "461 FILE END :Not enough parameters\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-        std::string filename = tokens[2];
-
-        std::string key = makeTransferKey(fd, filename);
-        if (server->getFileTransfers().count(key) == 0) {
-            std::string err = "400 :No such file transfer session\r\n";
-            send(fd, err.c_str(), err.size(), 0);
-            return;
-        }
-
-        FileTransfer &ft = server->getFileTransfers()[key];
-        bool complete = ft.isComplete();
-        int receiverFd = ft.getReceiverFd();
-
-        // Notify sender
-        if (!complete) {
-            std::string msgSender = "NOTICE " +
-                server->getClients()[fd]->getNickname() +
-                " :File transfer ended, but file is incomplete (" +
-                std::to_string(ft.getReceivedBytes()) + "/" +
-                std::to_string(ft.getFilesize()) + ")\r\n";
-            send(fd, msgSender.c_str(), msgSender.size(), 0);
-        } else {
-            std::string msgSender = "NOTICE " +
-                server->getClients()[fd]->getNickname() +
-                " :File transfer completed (" + ft.getFilename() + ")\r\n";
-            send(fd, msgSender.c_str(), msgSender.size(), 0);
-        }
-
-        // Send the file to the receiver (for demonstration).
-        {
-            const std::vector<char>& fileBuf = ft.getFileBuffer();
-
-            // A short info message to the receiver
-            std::ostringstream oss;
-            oss << "NOTICE "
-                << server->getClients()[receiverFd]->getNickname()
-                << " :You have received file ["
-                << ft.getFilename()
-                << "] with size " << fileBuf.size() << " bytes\r\n";
-            std::string infoMsg = oss.str();
-            send(receiverFd, infoMsg.c_str(), infoMsg.size(), 0);
-
-            // In a real scenario, we'd prefer DCC or other method for big files.
-            // Here, we just do a direct send of raw data (which may look like garbage
-            // in an IRC client). One could base64 it back, or do chunked sends.
-            if (!fileBuf.empty()) {
-                send(receiverFd, &fileBuf[0], fileBuf.size(), 0);
-            }
-        }
-
-        // Remove the transfer session
-        server->getFileTransfers().erase(key);
+        handleFileEnd(server, fd, tokens);
     }
-
-    // ----------------------
-    // Unknown subcommand
-    // ----------------------
     else {
         std::string err = "400 :Unknown FILE subcommand\r\n";
         send(fd, err.c_str(), err.size(), 0);
     }
 }
 
-/**
- * @brief Minimal base64 decode logic. 
- *        This ignores invalid characters outside base64 and '=' for padding.
- */
+/* ------------------------------------------------------------------
+ * BASE64 DECODING
+ * ------------------------------------------------------------------ */
+
 static const std::string base64Chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz"
@@ -258,6 +279,10 @@ static inline bool isBase64(unsigned char c)
     return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
+/**
+ * @brief Minimal base64 decode logic. 
+ *        This ignores invalid characters outside base64 and '=' for padding.
+ */
 static std::vector<char> base64Decode(const std::string &encodedString)
 {
     int inLen = static_cast<int>(encodedString.size());
@@ -275,7 +300,7 @@ static std::vector<char> base64Decode(const std::string &encodedString)
         in_++;
         if (i == 4) {
             for (i = 0; i < 4; i++) {
-                charArray4[i] =
+                charArray4[i] = 
                     static_cast<unsigned char>(base64Chars.find(charArray4[i]));
             }
 
@@ -298,7 +323,7 @@ static std::vector<char> base64Decode(const std::string &encodedString)
             charArray4[j] = 0;
         }
         for (j = 0; j < 4; j++) {
-            charArray4[j] =
+            charArray4[j] = 
                 static_cast<unsigned char>(base64Chars.find(charArray4[j]));
         }
 
