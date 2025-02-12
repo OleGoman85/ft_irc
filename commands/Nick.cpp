f18600de24f1d856e27eee91257e4b57720c2f4d
Alisa Arbenina <aarbenina@gmail.com>
2025-02-12 13:21:20 +0200

    [Alisa] Nick - 60%

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -6,41 +6,47 @@
 /*   By: aarbenin <aarbenin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 11:30:54 by ogoman            #+#    #+#             */
-/*   Updated: 2025/02/11 14:46:12 by aarbenin         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:05:54 by aarbenin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Nick.hpp"
-
-#include <string>
+#include <algorithm>  // std::transform
 
 #include "../include/Replies.hpp"
 #include "../include/Server.hpp"
 
-/**
- * @brief Handles the NICK command from a client.
- *
- * This function processes the NICK command, which is used by a client to set or
- * change its nickname. The function first verifies that a nickname was
- * provided. It then checks if the desired nickname is already in use by another
- * client. If the nickname is available, the client's nickname is updated.
- * Additionally, if the client's authentication state indicates that they are
- * waiting for the USER command, their state is updated accordingly. A
- * confirmation message is then sent to the client.
- *
- * @param server Pointer to the Server object managing the IRC server.
- * @param fd The file descriptor of the client issuing the NICK command.
- * @param tokens A vector of strings representing the tokenized command
- * arguments (expected: "NICK" followed by <newNick>).
- * @param command The complete command string received from the client (unused
- * in this implementation).
- */
+static void broadcastNickChange(Server* server, Client* client,
+                                const std::string& oldNick,
+                                const std::string& newNick)
+{
+    std::string message = ":" + oldNick + "!" + client->getUsername() + "@" +
+                          client->getHost() + " NICK :" + newNick + "\r\n";
+
+    int fd = client->getFd();
+    for (std::map<std::string, Channel>::iterator it =
+             server->getChannels().begin();
+         it != server->getChannels().end(); ++it)
+    {
+        Channel& chan = it->second;
+        if (chan.hasClient(fd))
+        {
+            const std::vector<int>& chanClients = chan.getClients();
+            for (size_t i = 0; i < chanClients.size(); i++)
+            {
+                int otherFd = chanClients[i];
+                send(otherFd, message.c_str(), message.size(), 0);
+            }
+        }
+    }
+}
+
 void handleNickCommand(Server* server, int fd,
                        const std::vector<std::string>& tokens,
                        const std::string& /*command*/)
 {
     if (tokens.size() < 2)
     {
+        // 431 ERR_NONICKNAMEGIVEN
         std::string reply = "431 :No nickname given\r\n";
         send(fd, reply.c_str(), reply.size(), 0);
         return;
@@ -50,8 +56,9 @@ void handleNickCommand(Server* server, int fd,
 
     for (const auto& pair : server->getClients())
     {
-        if (pair.second->getNickname() == newNick)
+        if (pair.first != fd && pair.second->getNickname() == newNick)
         {
+            // 433 ERR_NICKNAMEINUSE
             std::string reply =
                 "433 * " + newNick + " :Nickname is already in use\r\n";
             send(fd, reply.c_str(), reply.size(), 0);
@@ -59,16 +66,47 @@ void handleNickCommand(Server* server, int fd,
         }
     }
 
-    server->getClients()[fd]->setNickname(newNick);
+    Client* client = server->getClients()[fd].get();
+    if (!client) return;
 
-    AuthState& st = server->getClients()[fd]->authState;
+    std::string oldNick = client->getNickname();
 
-    if (st == WAITING_FOR_NICK) st = WAITING_FOR_USER;
+    client->setNickname(newNick);
 
-    Client* c = server->getClients()[fd].get();
-    if (st == WAITING_FOR_USER && !c->getUsername().empty())
+    AuthState& st = client->authState;
+
+    if (st == NOT_REGISTERED)
     {
-        st = AUTH_REGISTERED;
-        sendWelcome(server, fd);
+        st = WAITING_FOR_USER;
+        return;
+    }
+
+    if (st == WAITING_FOR_NICK)
+    {
+        st = WAITING_FOR_USER;
+        if (!client->getUsername().empty())
+        {
+            st = AUTH_REGISTERED;
+            sendWelcome(server, fd);
+        }
+        return;
+    }
+
+    if (st == WAITING_FOR_USER)
+    {
+        if (!client->getUsername().empty())
+        {
+            st = AUTH_REGISTERED;
+            sendWelcome(server, fd);
+        }
+        return;
+    }
+
+    if (st == AUTH_REGISTERED)
+    {
+        if (oldNick != newNick && !oldNick.empty())
+        {
+            broadcastNickChange(server, client, oldNick, newNick);
+        }
     }
 }
