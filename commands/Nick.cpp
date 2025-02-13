a00aad15b798f543fbfd9e0b1791b1e3540ade35
Alisa Arbenina <aarbenina@gmail.com>
2025-02-13 06:40:24 +0200

    [Alisa] Nick - 95% 😬

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -1,33 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Nick.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aarbenin <aarbenin@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/20 11:30:54 by ogoman            #+#    #+#             */
-/*   Updated: 2025/02/12 13:35:37 by aarbenin         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <algorithm>  // std::transform
+#include <set>        // std::set
 
 #include "../include/Replies.hpp"
 #include "../include/Server.hpp"
 
+/**
+ * @brief Sends a nickname change notification to all clients in shared channels.
+ *
+ * This function ensures that a client receives the nickname change message only once,
+ * even if they are in multiple channels.
+ *
+ * @param server Pointer to the Server instance.
+ * @param client Pointer to the Client whose nickname changed.
+ * @param oldNick The previous nickname.
+ * @param newNick The new nickname.
+ */
 static void broadcastNickChange(Server* server, Client* client, 
                                 const std::string& oldNick, 
                                 const std::string& newNick)
 {
-
     std::string message =
-        ":" + oldNick + "!" 
-        + client->getUsername() + "@" + client->getHost() 
-        + " NICK :" + newNick + "\r\n";
+        ":" + oldNick + "!" + client->getUsername() + "@" + client->getHost() +
+        " NICK :" + newNick + "\r\n";
 
     int fd = client->getFd();
-
-    send(fd, message.c_str(), message.size(), 0);
+    
+    // Use a set to avoid duplicate sends (e.g., if a user is in multiple channels).
+    std::set<int> notifiedClients;
 
     for (std::map<std::string, Channel>::iterator it = server->getChannels().begin();
          it != server->getChannels().end(); ++it)
@@ -35,24 +34,37 @@ static void broadcastNickChange(Server* server, Client* client,
         Channel& chan = it->second;
         if (chan.hasClient(fd))
         {
-            const std::vector<int>& chanClients = chan.getClients();
-            for (size_t i = 0; i < chanClients.size(); i++)
+            for (int otherFd : chan.getClients())
             {
-                int otherFd = chanClients[i];
-                send(otherFd, message.c_str(), message.size(), 0);
+                if (otherFd != fd && notifiedClients.insert(otherFd).second)
+                {
+                    send(otherFd, message.c_str(), message.size(), 0);
+                }
             }
         }
     }
+
+    // Send the nickname change message directly to the user
+    send(fd, message.c_str(), message.size(), 0);
 }
 
-
+/**
+ * @brief Handles the NICK command from a client.
+ *
+ * This function ensures that a nickname is unique, changes it if valid,
+ * and notifies all relevant clients.
+ *
+ * @param server Pointer to the Server instance.
+ * @param fd The client's file descriptor.
+ * @param tokens The parsed command tokens.
+ * @param command The full command string (unused).
+ */
 void handleNickCommand(Server* server, int fd,
                        const std::vector<std::string>& tokens,
                        const std::string& /*command*/)
 {
     if (tokens.size() < 2)
     {
-        // 431 ERR_NONICKNAMEGIVEN
         std::string reply = "431 :No nickname given\r\n";
         send(fd, reply.c_str(), reply.size(), 0);
         return;
@@ -64,9 +76,7 @@ void handleNickCommand(Server* server, int fd,
     {
         if (pair.first != fd && pair.second->getNickname() == newNick)
         {
-            // 433 ERR_NICKNAMEINUSE
-            std::string reply =
-                "433 * " + newNick + " :Nickname is already in use\r\n";
+            std::string reply = "433 * " + newNick + " :Nickname is already in use\r\n";
             send(fd, reply.c_str(), reply.size(), 0);
             return;
         }
@@ -108,11 +118,8 @@ void handleNickCommand(Server* server, int fd,
         return;
     }
 
-    if (st == AUTH_REGISTERED)
+    if (st == AUTH_REGISTERED && !oldNick.empty() && oldNick != newNick)
     {
-        if (oldNick != newNick && !oldNick.empty())
-        {
-            broadcastNickChange(server, client, oldNick, newNick);
-        }
+        broadcastNickChange(server, client, oldNick, newNick);
     }
 }
